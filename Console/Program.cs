441a364aeea5c6d61d65ad3caa893ad419add6cc
risaUp <100235473+risaUp@users.noreply.github.com>
2022-03-06 18:12:13 +0300

    Добавьте файлы проекта.

---
+++ Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ClassLibrary;
+
+namespace ConsoleE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = GetValueFromUser("Введите целое положительное число.");
+            N.SquareStars();
+            N = GetValueFromUser("Введите длину квадрата. ");
+            N.ОrderNum();
+        }
+
+        private static int GetValueFromUser(string message)
+        {
+            Console.WriteLine(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ MyLib/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    ///<summary>
+    ///вычисления на основе заданых значений
+    ///</summary>
+    public static class StringLibrary
+    {
+        ///<summary>
+        ///Вывод квадрата звёзд с пустым центром
+        ///</summary>
+        public static void SquareStars(this int N)
+        {
+
+            for (int i = 0; i <= N; i++)
+            {
+                for (int j = 0; j <= N; i++)
+                {
+                    if ((i == j) && (i == (N / 2))) {
+                        Console.Write(" ");
+                    }
+                    else
+
+                        Console.Write("*");
+                }
+            }
+        }
+
+        public static void ОrderNum(this int N)
+        {
+
+        for (int i = 1; i <= N; i++)
+        {
+            if (i == N)
+            {
+                Console.Write(i + ".");
+            }
+            else
+            {
+                Console.Write(i + ", ");
+            }
+        }
+        }
+    }
+}
+
