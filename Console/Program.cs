38af45519d7bf263bfdebab0e03953389bab94c1
risaUp <100235473+risaUp@users.noreply.github.com>
2022-03-06 18:12:13 +0300

    Добавьте файлы проекта.

---
+++ Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MyLib;
+
+namespace ConsoleE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите целое положительное число");
+            int? N = Convert.ToInt32(Console.ReadLine());
+            N.WriteNum();
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Введите целое нечетное положительное число");
+            N = Convert.ToInt32(Console.ReadLine());
+            N.WriteStars();
+        }
+    }
+}
---
+++ MyLib/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace MyLib
+{
+    public static class StringLibrary
+    {
+        public static void WriteStars(this int? N)
+        {
+
+
+            for (int i = 1; i <= N; i++)
+            {
+                int result = (int)((N + 1) / 2);
+                if (i == result)
+                {
+                    for (int j = 1; j < result; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.Write(" ");
+                    for (int j = 1; j < result; j++)
+                    {
+                        Console.Write("*");
+                    }
+                }
+                else
+                {
+                    for (int j = 1; j <= N; j++)
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.Write("\n");
+            }
+        }
+
+        public static void WriteNum(this int? N)
+        {
+
+            for (int i = 1; i <= N; i++)
+            {
+                if (i == N)
+                {
+                    Console.Write(i + ".");
+                }
+                else
+                {
+                    Console.Write(i + ", ");
+                }
+            }
+        }
+
+    }
+}
